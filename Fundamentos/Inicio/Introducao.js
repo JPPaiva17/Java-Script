47475a849a1de438c54ec725d4685baaca449fc7
João Pedro Paiva <jppaiva1706@gmail.com>
2025-01-09 20:15:34 +0100

    Introducao
    Introdução a linguagem JavaScript

---
+++ Fundamentos/Inicio/Introducao.js
@@ -0,0 +1,25 @@
+// Conceitos iniciais a respeito de JavaScript
+
+// Teste de print, nesse caso é console.log
+
+console.log("Joao");
+console.log("1");
+console.log(777);
+console.log(true);
+console.log(" ")
+
+// Imprimindo pares de 1 a 10
+
+for(i = 0; i<=10; i+=2){
+    console.log(i);
+}
+console.log(" ")
+
+// Imprimindo impares de 1 a 10
+
+for(i = 1; i<=10; i+=2){
+    console.log(i);
+}
+console.log(" ")
+
+
