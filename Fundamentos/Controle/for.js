4255ad059cc2fc848d132de644830e5a0cf4148b
João Pedro Paiva <jppaiva1706@gmail.com>
2025-01-27 19:06:55 +0100

    For

---
+++ Fundamentos/Controle/for.js
@@ -0,0 +1,9 @@
+for(let i = 1; i<=10 ; i++){
+    console.log(`i= ${i}`);
+}
+
+const notas = [1.2, 5.4, 7.7, 10, 1, 4.9]
+
+for(let i = 0; i<notas.length; i++){
+    console.log(`Notas= ${notas[i]}`);
+}
