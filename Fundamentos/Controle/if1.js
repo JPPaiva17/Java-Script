85fa6990791daeac4cb9019897eb9d7001e23de7
João Pedro Paiva <jppaiva1706@gmail.com>
2025-01-27 18:10:43 +0100

    Treino Basico if else

---
+++ Fundamentos/Controle/if1.js
@@ -0,0 +1,11 @@
+    function soNoticiaBoa (nota){
+        if(nota>= 7){
+            console.log("Aprovado com nota ", nota);
+        }
+        else{
+            console.log("Reprovado com nota ", nota);
+        }
+    }
+
+    soNoticiaBoa(8.1)
+    soNoticiaBoa(6)
