78841cdc0ed73da1c0cd1157cbca5e6939989f07
João Pedro Paiva <jppaiva1706@gmail.com>
2025-02-19 14:52:46 +0100

    Introdução à Orientação a Objetos

---
+++ Objetos/oo.js
@@ -0,0 +1,29 @@
+/*
+
+// Procedural
+
+processamento(valor1, valor2, valor3...){
+}  -> passando valores para procedimentos que serão manipulados
+
+// Orientação à Objetos
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+    }
+}  ->   Agora um objeto possui seus atributos e suas funções 
+        que sao invocadas a partir desse objetos
+
+
+objeto.processamento()  // Foco passou a ser o objeto
+
+// Principios importantes:
+
+1. Abstração
+2. Encapsulamento
+3. Herança (prototype)
+4. Polimorfismo
+
+*/
