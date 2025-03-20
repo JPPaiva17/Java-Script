6c954658fa2f438d61324f2ed542f5ab4f27f013
João Pedro Paiva <jppaiva1706@gmail.com>
2025-03-20 11:51:33 -0300

    for of & promises

---
+++ ESNext/forOf.js
@@ -0,0 +1,16 @@
+for(let letras of 'Cod3r'){
+    console.log(letras)
+}
+
+const assuntosEcma = ['Map', 'Set', 'Promise']
+
+console.log('\n// for in: \n')
+for(let i in assuntosEcma){
+    console.log(i)
+}
+
+
+console.log('\n// for of: \n')
+for(let i of assuntosEcma){
+    console.log(i)
+}
